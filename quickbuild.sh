d585b707a8b1bd4e5f0a8fd2e9af3af68cbbced4
Mattias Schlenker <ms@mattiasschlenker.de>
2014-06-20 11:03:52 +0200

    add script to quickly build

--- quickbuild.sh
+++ quickbuild.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# Script to do a quick and dirty default build of LessLinux (unstable)
-# Standard paths are used. Stage02 is built with three threads. An ext4
+# Script to do a quick and dirty default build of LessLinux Standard
+# paths are used. Stage02 is built with four threads. An ext4
 # or btrfs mounted disk at /mnt/archiv or /mnt/archiv/LessLinux is 
 # expected. 
 #
@@ -40,6 +40,24 @@ if [ ` cat /proc/swaps | wc -l ` -lt 2 ] ; then
 	read swap
 fi
 
+echo "How many threads do you wand to use? Default: 4"
+read numthreads
+if [ "$numthreads" -gt 0 ] ; 
+	echo "Valid input..."
+else
+	numthreads=4
+fi
+echo "Do you want to build the unstable branch? Default: Y"
+read unstbl
+case $unstbl in
+	[Nn]*)
+		unstable=""
+	;;
+	*)
+		unstable="-u"
+	;;
+esac
+
 mkdir -p /mnt/archiv/LessLinux/llbuild
 mkdir -p /mnt/archiv/LessLinux/src
 
@@ -52,7 +70,7 @@ else
 	echo "Building stage01 - please be patient."
 	touch /mnt/archiv/LessLinux/llbuild/stage01.log
 	Terminal --hide-menubar -T "LOG: stage01" -e "tail -f /mnt/archiv/LessLinux/llbuild/stage01.log" &
-	ruby -I. builder.rb -s 2,3 -n -l -t 4 -u --no-stracalyze --ignore-arch  >> /mnt/archiv/LessLinux/llbuild/stage01.log 2>&1 
+	ruby -I. builder.rb -s 2,3 -n -l -t 4 $unstable --no-stracalyze --ignore-arch  >> /mnt/archiv/LessLinux/llbuild/stage01.log 2>&1 
 	echo '' >> /mnt/archiv/LessLinux/llbuild/stage01.log
 	echo 'You might close the log.' >> /mnt/archiv/LessLinux/llbuild/stage01.log
 fi
@@ -62,7 +80,7 @@ if [ -f /mnt/archiv/LessLinux/llbuild/stage01.tar.xz ] ; then
 	echo "Building stage02 - please be patient."
         touch /mnt/archiv/LessLinux/llbuild/stage02.log
         Terminal --hide-menubar -T "LOG: stage02" -e "tail -f /mnt/archiv/LessLinux/llbuild/stage02.log" &
-        ruby -I. builder.rb -s 1,3 -n -l -t 4 -u --no-stracalyze --ignore-arch  >> /mnt/archiv/LessLinux/llbuild/stage02.log 2>&1
+        ruby -I. builder.rb -s 1,3 -n -l -t 4 $unstable --no-stracalyze --ignore-arch  >> /mnt/archiv/LessLinux/llbuild/stage02.log 2>&1
         echo 'Done! Please check the log. Did everything succeed?'
 	echo 'If it did: Read the instructions on how to build a bootable ISO image!'
 else
