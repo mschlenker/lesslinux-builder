6f918b652e29a95b69d70d5b7153cde4d8077bc1
Mattias Schlenker <ms@mattiasschlenker.de>
2014-06-23 09:39:45 +0200

    add overlays for search and rescue

---
+++ overlays/search_and_rescue/bootdisk/boot/isolinux/common/sifomenu.cfg.pl
@@ -0,0 +1,20 @@
+MENU INCLUDE /boot/isolinux/common/colors.cfg
+MENU INCLUDE /boot/isolinux/common/title.cfg
+TIMEOUT 100
+
+MENU INCLUDE /boot/isolinux/pl/sifo/default.cfg
+MENU BEGIN sfenpaem
+	MENU TITLE Safe startup
+	LABEL back
+		MENU LABEL Go back
+		MENU EXIT
+	MENU INCLUDE /boot/isolinux/pl/sifo/safemode.cfg
+MENU END
+LABEL memtest
+MENU LABEL Test your memory
+KERNEL /boot/isolinux/memtest.bzi
+# APPEND initrd=/boot/isolinux/memtest.img
+MENU INCLUDE /boot/isolinux/pl/boot0x80.cfg
+# MENU INCLUDE /boot/isolinux/pl/usbonly.cfg
+MENU INCLUDE /boot/isolinux/common/sifolang.cfg
+
---
+++ overlays/search_and_rescue/efiimage/loader/entries/63less.conf.es
@@ -0,0 +1,5 @@
+title LL Search and Rescue (Reverse VNC + local GUI)
+linux    /EFI/BOOT/LINUX.EFI
+initrd   /idefsf.gz
+options  ultraquiet=2 security=none zswap.enabled=1 skipcheck=1 quiet lang=es ejectonumass=1 skipservices=|installer|xconfgui|roothash|firewall|ssh|mountdrives| hwid=unknown laxsudo=1 toram=1000000 optram=1 swap=none swapsize=0000 uuid=all crypt=all homecont=0000-00000 console=tty2 loop.max_loop=32  x11vnc=|reverse|xyz.domain.abc|
+ 
