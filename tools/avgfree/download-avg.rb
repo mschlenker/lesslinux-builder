e1af797c87be9aa1a8ecd870130e554b49606172
Mattias Schlenker <ms@mattiasschlenker.de>
2014-09-17 16:59:48 +0200

    integrate avg into new blob schematics

---
+++ tools/avgfree/download-avg.rb
@@ -0,0 +1,40 @@
+#!/usr/bin/ruby
+# encoding: utf-8
+
+require 'net/http'
+
+server = 'free.avg.com'
+path = '/de-de/download-free-all-product'
+
+page = Net::HTTP.get(server, path)
+
+exit 1 if page.nil? 
+
+tarball = nil
+
+ptoks = page.split
+ptoks.each { |t|
+	if t =~ /href\=\"(.*)\"$/
+		url = $1
+		if url =~ /flx(.*?)\.tar.gz$/ 
+			puts url 
+			tarball = url
+		end
+	end
+}
+
+exit 1 if tarball.nil? 
+
+if system("mountpoint -q /lesslinux/blobpart") 
+	if system("wget -O /lesslinux/blobpart/avg-free.tgz \"#{tarball}\"")
+		system("ln -sf /lesslinux/blobpart/avg-free.tgz /lesslinux/blob/")
+	else
+		system("rm /lesslinux/blobpart/avg-free.tgz")
+		exit 1
+	end
+else
+	unless system("wget -O /lesslinux/blob/avg-free.tgz \"#{tarball}\"")
+		system("rm /lesslinux/blob/avg-free.tgz")
+		exit 1
+	end
+end
