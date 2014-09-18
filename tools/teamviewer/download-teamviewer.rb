f0f75745896d383557f4da35b264efc0c4f28a59
Mattias Schlenker <ms@mattiasschlenker.de>
2014-09-18 12:31:45 +0200

    add blobinstall for teamviewer

---
+++ tools/teamviewer/download-teamviewer.rb
@@ -0,0 +1,20 @@
+#!/usr/bin/ruby
+# encoding: utf-8
+
+dlurl = "http://downloadeu1.teamviewer.com/download/teamviewer_linux.tar.gz"
+
+if system("mountpoint -q /lesslinux/blobpart") 
+	if system("wget -O /lesslinux/blobpart/teamviewer_linux.tar.gz #{dlurl}")
+		system("ln -sf /lesslinux/blobpart/teamviewer_linux.tar.gz /lesslinux/blob/")
+	else
+		system("rm /lesslinux/blobpart/teamviewer_linux.tar.gz")
+		exit 1
+	end
+else
+	unless system("wget -O /lesslinux/blob/teamviewer_linux.tar.gz #{dlurl}")
+		system("rm /lesslinux/blob/teamviewer_linux.tar.gz")
+		exit 1
+	end
+end
+
+system("/etc/rc.d/0600-teamviewer.sh start") 
