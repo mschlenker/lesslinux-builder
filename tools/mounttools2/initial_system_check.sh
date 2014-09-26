376ef4d95f2d531989551101fed4e9f86d96299a
Mattias Schlenker <ms@mattiasschlenker.de>
2014-09-26 15:53:25 +0200

    run initial system check only once

--- tools/mounttools2/initial_system_check.sh
+++ tools/mounttools2/initial_system_check.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 # encoding: utf-8
 
-ruby -I /usr/share/lesslinux/drivetools /usr/share/lesslinux/drivetools/initial_system_check.rb 
+if [ -f /var/run/initial_check.done ] ; then
+	echo 'Skipping check...'
+else
+	ruby -I /usr/share/lesslinux/drivetools /usr/share/lesslinux/drivetools/initial_system_check.rb 
+	touch /var/run/initial_check.done
+fi
