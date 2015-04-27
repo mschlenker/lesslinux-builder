58dfcf27936ce8c32fdd427e0d5b8c9e22d4ca64
Mattias Schlenker <ms@mattiasschlenker.de>
2015-04-27 11:32:35 +0200

    start moving to new library

--- tools/lesslinux-emergencytools/photorec-starter.rb
+++ tools/lesslinux-emergencytools/photorec-starter.rb
@@ -4,6 +4,8 @@
 require 'glib2'
 require 'gtk2'
 require "rexml/document"
+require 'MfsDiskDrive'
+require 'MfsSinglePartition'
 
 # License? GPL v2.0
 
@@ -179,325 +181,31 @@ def read_suffixes
 	return suffixes
 end
 
-def read_devs
-	xml_obj = nil
-	if ARGV.size > 0 
-		file = File.new( ARGV[0] )
-		doc = REXML::Document.new(file)
-		$dummy = true
-	else	
-		system("sudo /sbin/mdev -s")
-		sleep 2
-		xmlstr = ""
-		IO.popen("sudo /usr/sbin/lshw-static -xml") { |x|
-			while x.gets
-				xmlstr = xmlstr +$_
-			end
-		}
-		doc = REXML::Document.new(xmlstr)
-	end
-	return doc
-end
 
-def scan_parts (doc)
-	alldisks = []
-	doc.elements.each("//node[@class='storage']") { |x|
-		begin
-			businfo = x.elements["businfo"].text.to_s
-		rescue
-			businfo = "unknown"
-		end
-		storageproduct = ""
-		begin
-			storageproduct = x.elements["vendor"].text.to_s + " " + x.elements["product"].text.to_s
-		rescue
-		end
-		x.elements.each("node[@class='disk']") { |element|
-			parts = []
-			product = ""
-			size = 0
-			unit = ""
-			cdrom = false
-			diskname = element.elements["logicalname"].text.to_s
-			begin
-				product = element.elements["product"].text.to_s
-			rescue
-				product = element.elements["description"].text.to_s 
-				product = storageproduct unless storageproduct == ""
+def scan_parts_ng
+	@drives = Array.new
+	alldisks = Array.new
+	Dir.entries("/sys/block").each { |l|
+			if l =~ /[a-z]$/ 
+				begin
+					d =  MfsDiskDrive.new(l, true)
+					@drives.push(d) 
+				rescue 
+					$stderr.puts "Failed adding: #{l}"
+				end
 			end
-			begin
-				size = element.elements["size"].text.to_i
-				unit = element.elements["size"].attributes["units"]
-			rescue
-			end
-			# puts element.attributes["handle"]  + " " + product + " " + businfo + " " + size.to_s + " " + unit
-			if element.attributes["id"] =~ /^cdrom/ 
-				#	puts "  " + element.elements["logicalname"].text
-				cdrom = true
-				mount_point = nil
-				log_name = []
-				element.elements.each("logicalname") { |l| log_name.push(l.text) }
-				state = "clear"
-				if log_name.size > 1
-					mount_point = log_name[1] 
-					state = "mounted"
-				end
-				parts.push( [ element.elements["logicalname"].text, "iso9660", state, mount_point, false, 0 ] )
-			end
-			element.elements.each("node[@class='volume']") { |n|
-				state = ""
-				mount_options = []
-				rw = false
-				begin
-					puts "Checking: " + n.elements["logicalname"].text
-					state = n.elements["configuration/setting[@id='state']"].attributes["value"].to_s
-					mount_options = n.elements["configuration/setting[@id='mount.options']"].
-						attributes["value"].to_s.split(",")
-					mount_options.each { |m| rw = true if m == "rw" }
-				rescue
-				end
-				fstype = nil
-				begin
-					fstype = n.elements["configuration/setting[@id='filesystem']"].attributes["value"].strip
-				rescue
-					fstype = n.elements["logicalname"].text
-				end
-				is_extended = false
-				begin
-					is_extended = true if n.elements["capabilities/capability[@id='partitioned:extended']"].text.strip == "Extended partition"
-				rescue
-				end
-				unless  is_extended == true # || fstype == "swap"
-					# puts "  " + n.attributes["id"] + " " + n.elements["logicalname"].text + " " + 
-					#	n.elements["configuration/setting[@id='filesystem']"].attributes["value"] + " " + state
-					mount_point = nil
-					log_name = []
-					capacity = 0
-					begin
-						capacity = n.elements["capacity"].text.to_i
-					rescue
-					end
-					n.elements.each("logicalname") { |l| log_name.push(l.text) }
-					mount_point = log_name[1] if log_name.size > 1
-					begin
-						parts.push( [ n.elements["logicalname"].text,
-						fstype.to_s,
-						state, mount_point, rw, capacity ] )
-					rescue
-					end
-				end
-				if is_extended == true
-					n.elements.each("node[@class='volume']") { |m|
-						begin
-							istate = ""
-							imount_options = []
-							irw = false
-							begin
-								istate = m.elements["configuration/setting[@id='state']"].attributes["value"].to_s
-								imount_options = m.elements["configuration/setting[@id='mount.options']"].
-									attributes["value"].to_s.split(",")
-								imount_options.each { |o| irw = true if o == "rw" }
-							rescue
-							end
-							ifstype = nil
-							begin
-								ifstype = m.elements["configuration/setting[@id='filesystem']"].attributes["value"].strip
-							rescue
-								ifstype = "unbekanntes Dateisystem"
-								m.elements.each("description") { |d|
-									puts "Checking: " + d.text
-									ifstype = "fat" if d.text =~ /FAT/
-									ifstype = "ntfs" if d.text =~ /NTFS/
- 									ifstype = "ext3" if d.text =~ /Linux/
-									if d.text =~ /swap/
-										ifstype = "swap"
-										fstype = "swap"
-									end
-								}
-							end
-							unless fstype == "swap"
-								mount_point = nil
-								log_name = []
-								capacity = 0
-								begin
-									capacity = m.elements["capacity"].text.to_i
-								rescue
-								end
-								m.elements.each("logicalname") { |l| log_name.push(l.text) }
-								mount_point = log_name[1] if log_name.size > 1
-								parts.push( [ m.elements["logicalname"].text,
-								ifstype.to_s,
-								istate, mount_point, rw, capacity ] )
-							end
-						rescue
-						end
-					}
-				end
-				
-			}
-			alldisks.push( [  businfo, product, size, unit, cdrom, parts, diskname ] )
-		}
-		# FIXME: This is weird copy&paste
-		x.elements.each("node[@class='disk']/node[@class='disk']") { |element|
-			parts = []
-			product = ""
-			size = 0
-			unit = ""
-			cdrom = false
-			diskname = element.elements["logicalname"].text.to_s
-			begin
-				product = element.elements["product"].text.to_s
-			rescue
-				# product = element.elements["description"].text.to_s
-				product = "unknown"
-			end
-			begin
-				size = element.elements["size"].text.to_i
-				unit = element.elements["size"].attributes["units"]
-			rescue
-			end
-			# puts element.attributes["handle"]  + " " + product + " " + businfo + " " + size.to_s + " " + unit
-			if element.attributes["id"] =~ /^cdrom/ 
-				#	puts "  " + element.elements["logicalname"].text
-				cdrom = true
-				mount_point = nil
-				log_name = []
-				element.elements.each("logicalname") { |l| log_name.push(l.text) }
-				state = "clear"
-				if log_name.size > 1
-					mount_point = log_name[1] 
-					state = "mounted"
-				end
-				parts.push( [ element.elements["logicalname"].text, "iso9660", state, mount_point, false, 0 ] )
-			end
-			element.elements.each("node[@class='volume']") { |n|
-				state = ""
-				mount_options = []
-				rw = false
-				begin
-					puts "Checking: " + n.elements["logicalname"].text
-					state = n.elements["configuration/setting[@id='state']"].attributes["value"].to_s
-					mount_options = n.elements["configuration/setting[@id='mount.options']"].
-						attributes["value"].to_s.split(",")
-					mount_options.each { |m| rw = true if m == "rw" }
-				rescue
-				end
-				fstype = nil
-				begin
-					fstype = n.elements["configuration/setting[@id='filesystem']"].attributes["value"].strip
-				rescue
-					fstype = extract_lang_string("unknown_fs")
-				end
-				is_extended = false
-				begin
-					is_extended = true if n.elements["capabilities/capability[@id='partitioned:extended']"].text.strip == "Extended partition"
-				rescue
-				end
-				unless is_extended == true # || fstype == "swap" 
-					# puts "  " + n.attributes["id"] + " " + n.elements["logicalname"].text + " " + 
-					#	n.elements["configuration/setting[@id='filesystem']"].attributes["value"] + " " + state
-					mount_point = nil
-					log_name = []
-					capacity = 0
-					begin
-						capacity = n.elements["capacity"].text.to_i
-					rescue
-					end
-					n.elements.each("logicalname") { |l| log_name.push(l.text) }
-					mount_point = log_name[1] if log_name.size > 1
-					parts.push( [ n.elements["logicalname"].text,
-					fstype.to_s,
-					state, mount_point, rw, capacity ] )
-				end
-				if is_extended == true
-					n.elements.each("node[@class='volume']") { |m|
-						begin
-							istate = ""
-							imount_options = []
-							irw = false
-							begin
-								istate = m.elements["configuration/setting[@id='state']"].attributes["value"].to_s
-								imount_options = m.elements["configuration/setting[@id='mount.options']"].
-									attributes["value"].to_s.split(",")
-								imount_options.each { |o| irw = true if o == "rw" }
-							rescue
-							end
-							ifstype = nil
-							begin
-								ifstype = m.elements["configuration/setting[@id='filesystem']"].attributes["value"].strip
-							rescue
-								ifstype = extract_lang_string("unknown_fs")
-							end
-							# unless fstype == "swap"
-							mount_point = nil
-							log_name = []
-							capacity = 0
-							begin
-								capacity = m.elements["capacity"].text.to_i
-							rescue
-							end
-							m.elements.each("logicalname") { |l| log_name.push(l.text) }
-							mount_point = log_name[1] if log_name.size > 1
-							parts.push( [ m.elements["logicalname"].text,
-							ifstype.to_s,
-							istate, mount_point, rw, capacity ] )
-							# end
-						rescue
-						end
-					}
-				end
-				
-			}
-			alldisks.push( [  businfo, product, size, unit, cdrom, parts, diskname ] )
-		}
-		x.elements.each("node[@class='volume']") { |element|
-			if element.elements["logicalname"].text =~ /^\/dev\/sd/ && businfo =~ /^usb/
-				diskname = element.elements["logicalname"].text.to_s
-				parts = []
-				product = ""
-				size = 0
-				unit = ""
-				cdrom = false
-				istate = ""
-				imount_options = []
-				irw = false
-				begin
-					istate = element.elements["configuration/setting[@id='state']"].attributes["value"].to_s
-					imount_options = element.elements["configuration/setting[@id='mount.options']"].
-					attributes["value"].to_s.split(",")
-					imount_options.each { |o| irw = true if o == "rw" }
-				rescue
-				end
-				ifstype = nil
-				begin
-					ifstype = element.elements["configuration/setting[@id='filesystem']"].attributes["value"].strip
-				rescue
-					ifstype = extract_lang_string("unknown_fs")
-				end
-				# udrive = UsbDrive.new(element.elements["logicalname"].text)
-				begin
-					product = x.elements["vendor"].text + " " + x.elements["product"].text
-				#	udrive.product = x.elements["product"].text
-				rescue
-				end
-				mount_point = nil
-				log_name = []
-				size = element.elements["size"].text.to_i
-				capacity = 0
-				begin
-					capacity = m.elements["capacity"].text.to_i
-				rescue
-				end
-				element.elements.each("logicalname") { |l| log_name.push(l.text) }
-				mount_point = log_name[1] if log_name.size > 1
-				parts.push( [ element.elements["logicalname"].text, ifstype.to_s, istate, mount_point, irw, capacity ] )
-				alldisks.push( [  businfo, product, size, unit, cdrom, parts, diskname ] )
-			end
-			#all_drives[element.elements["logicalname"].text] = udrive unless 
-			#all_drives.has_key?( element.elements["logicalname"].text )
-		}
 	}
-	return alldisks
+	Dir.entries("/sys/block").each { |l|
+			if ( l =~ /mmcblk[0-9]$/ ||  l =~ /mmcblk[0-9][0-9]$/ )
+				begin 
+					d =  MfsDiskDrive.new(l, true)
+					@drives.push(d) 
+				rescue 
+					$stderr.puts "Failed adding: #{l}"
+				end
+			end
+	}
+	return @drives
 end
 
 def update_partcombo(disks, partcombo, partrows, drivecombo, driverows)
@@ -880,7 +588,8 @@ assi.set_forward_page_func{|curr|
 	if curr == 0
 		# when switching from first to second screen, read drivelist
 		puts "current screen 0, rereading drivelist"
-		alldisks = scan_parts(read_devs) if xml_read_count < 1
+		# alldisks = scan_parts(read_devs) if xml_read_count < 1
+		alldiskss = scan_parts_ng 
 		xml_read_count += 1
 		alldisks.each { |d|
 			# d[0] businfo
