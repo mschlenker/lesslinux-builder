42274b6508a758d77da0ec705649c49991572326
Mattias Schlenker <ms@mattiasschlenker.de>
2016-10-31 10:00:29 +0100

    download and install eset

---
+++ tools/eset/download-eset.rb
@@ -0,0 +1,23 @@
+#!/usr/bin/ruby
+# encoding: utf-8
+
+require 'fileutils'
+
+dlurl = "https://download.eset.com/com/eset/tools/recovery/rescue_cd/latest/eset-sysrescue.1.0.9.0.enu.iso"
+
+if system("mountpoint -q /lesslinux/blobpart") 
+	FileUtils.mkdir_p("/lesslinux/blobpart/eset")
+	unless system("wget -O /lesslinux/blobpart/eset/eset.iso #{dlurl}")
+		system("rm /lesslinux/blobpart/eset/eset.iso")
+		exit 1
+	end
+else
+	FileUtils.mkdir_p("/lesslinux/blob/eset")
+	unless system("wget -O /lesslinux/blob/eset/eset.iso #{dlurl}")
+		system("rm /lesslinux/blob/eset/eset.iso")
+		exit 1
+	end
+end
+
+system("/etc/rc.d/0611-eset.sh start") 
+
---
+++ tools/eset/eset.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# encoding: utf-8
+
+# Wrapper script for ESET virus scan
+# As non free component ESETs squashfs image is needed
+
+PATH=/usr/bin:/usr/sbin:/bin:/sbin:/static/bin:/static/sbin
+export PATH
+
+. /etc/rc.defaults
+. /etc/rc.subr/extractbootparams
+. /etc/rc.subr/colors
+. /etc/lesslinux/branding/branding.en.sh
+[ -f "/etc/lesslinux/branding/branding.${lang}.sh" ] && . /etc/lesslinux/branding/branding.${lang}.sh
+. /etc/rc.lang/en/messages.sh
+[ -f "/etc/rc.lang/$lang/messages.sh" ] && . /etc/rc.lang/$lang/messages.sh
+
+esnotfound="Could not find ESETs filesystem image - make sure you have an internet connection to start the installation now."
+[ $lang = de ] && esnotfound="Konnte ESETs Dateisystem-Image nicht finden - Stellen Sie bitte sicher, dass Sie über eine Internetverbindung verfügen, um die Installation jetzt zu starten."
+
+if [ '!' -x "/opt/eset/esets/sbin/esets_daemon" ] ; then
+	zenity --error --text "$esnotfound" 
+	sudo /usr/bin/blobinstall.sh --check eset
+fi
+sleep 2
+sudo /opt/eset/esets/bin/esets_gui
