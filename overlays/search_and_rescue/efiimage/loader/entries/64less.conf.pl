fb38d36ccdbd43669077d9256b0af37fee42443d
Mattias Schlenker <ms@mattiasschlenker.de>
2017-08-04 12:58:02 +0200

    rely on connman for network auto configuration

--- overlays/search_and_rescue/efiimage/loader/entries/64less.conf.pl
+++ overlays/search_and_rescue/efiimage/loader/entries/64less.conf.pl
@@ -1,5 +1,5 @@
 title LL Search and Rescue (Reverse VNC, no local GUI)
 linux    /EFI/BOOT/LINUX.EFI
 initrd   /idefsf.gz
-options  ultraquiet=3 security=none zswap.enabled=1 skipcheck=1 quiet lang=pl ejectonumass=1 skipservices=|installer|xconfgui|roothash|firewall|ssh|mountdrives| hwid=unknown laxsudo=1 toram=1000000 optram=1 swap=none swapsize=0000 blobsize=2048 uuid=all crypt=all homecont=0000-00000 console=tty2 loop.max_loop=32  x11vnc=|reverse|xyz.domain.abc| xvfb=1280x800x24 
+options  ultraquiet=3 security=none zswap.enabled=1 skipcheck=1 quiet lang=pl ejectonumass=1 skipservices=|earlynet|dhcpcd|installer|xconfgui|roothash|firewall|ssh|mountdrives| hwid=unknown laxsudo=1 toram=1000000 optram=1 swap=none swapsize=0000 blobsize=2048 uuid=all crypt=all homecont=0000-00000 console=tty2 loop.max_loop=32  x11vnc=|reverse|xyz.domain.abc| xvfb=1280x800x24 
 
