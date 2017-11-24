35522a9eba12dcc2660a9165c703c5cfee2e2625
Mattias Schlenker <ms@mattiasschlenker.de>
2017-11-24 16:24:14 +0100

    add experimental tool to connect bluetooth mice and keyboards

---
+++ tools/mounttools2/bluemousetool.rb
@@ -0,0 +1,149 @@
+#!/usr/bin/ruby
+# encoding: utf-8
+
+require 'glib2'
+require 'gtk2'
+require 'vte'
+
+@busydevices = Array.new 
+@lastline = 0
+@lastcol = 0 
+@pairdevice = nil
+
+def connect_everything(term, label)
+	term.feed_child("power on\n")
+	term.feed_child("agent on\n")
+	term.feed_child("default-agent\n")
+	term.feed_child("pairable on\n")
+	term.feed_child("scan on\n")
+	# Run for max 30 minutes
+	i = 1
+	while i < 3600 
+		while (Gtk.events_pending?)
+			Gtk.main_iteration
+		end
+		c,r = term.cursor_position
+		puts c.to_s + " " + r.to_s 
+		if r > @lastline 
+			numrows = r - @lastline
+			txt = term.get_text_range( r - numrows - 1, 0, r - 1, 79, false) 
+			inline = term.get_text_range( r, 0, r , 79, false) 
+			analyze_buffer(txt, inline, term, label)
+			@lastline = r 
+		end	
+		if i % 120 == 0
+			term.feed_child("scan on\n")
+		end
+		sleep 0.5
+		i += 1
+	end
+end
+
+def analyze_buffer(txt, inline, term, label)
+	# @pairdevice 
+	puts "Text:\n" + txt
+	puts "Input line:\n" + inline
+	lines = txt.split("\n") 
+	if inline =~ /Confirm passkey/ 
+		term.feed_child("no\n")
+		while (Gtk.events_pending?)
+			Gtk.main_iteration
+		end
+	end
+	if inline =~ /Enter PIN code/
+		term.feed_child("000000\n")
+		while (Gtk.events_pending?)
+			Gtk.main_iteration
+		end
+	end
+	lines.each{ |l|
+		if l =~ /Passkey\:\s([0-9]+)/ 
+			label.set_markup("<b>Found bluetooth keyboard, please enter passkey: <big>#{$1}</big></b>")
+		end
+		if l =~ /Pairing successful/
+			term.feed_child("trust #{@pairdevice}\n")
+			sleep 1.0
+			term.feed_child("connect #{@pairdevice}\n")
+			while (Gtk.events_pending?)
+				Gtk.main_iteration
+			end
+		end
+		if l =~ /Icon\:\sinput-mouse/
+			label.text = "Found mouse, trying to connect #{@pairdevice}"
+			term.feed_child("pair #{@pairdevice}\n")
+			while (Gtk.events_pending?)
+				Gtk.main_iteration
+			end
+		end
+		if l =~ /Icon\:\sinput-keyboard/
+			label.text = "Found keyboard, trying to connect #{@pairdevice}"
+			term.feed_child("pair #{@pairdevice}\n")
+			while (Gtk.events_pending?)
+				Gtk.main_iteration
+			end
+		end
+		if l =~ /^\[NEW\]/ 
+			@pairdevice = l.split[2]
+			term.feed_child("info #{@pairdevice}\n")
+			while (Gtk.events_pending?)
+				Gtk.main_iteration
+			end
+		end
+		if l =~ /Enter PIN code/
+			term.feed_child("000000\n")
+			while (Gtk.events_pending?)
+				Gtk.main_iteration
+			end
+		end
+	}
+end
+
+
+lang = ENV['LANGUAGE'][0..1]
+lang = ENV['LANG'][0..1] if lang.nil?
+lang = "en" if lang.nil?
+#tlfile = "chntpwgui.xml"
+#tlfile = "/usr/share/lesslinux/drivetools/chntpwgui.xml" if File.exists?("/usr/share/lesslinux/drivetools/chntpwgui.xml")
+#tl = MfsTranslator.new(lang, tlfile)
+#@tl = tl
+
+msg_label = Gtk::Label.new
+msg_label.wrap = true
+msg_label.width_request = 300
+msg_label.text = "Waiting for bluetooth"
+entry = Gtk::Entry.new
+entry.width_request = 300 
+
+vte = Vte::Terminal.new
+# vte.width_request = 600
+# vte.height_request = 320 
+vte.set_size(80, 25)  
+puts vte.scrollback_lines 
+vte.set_scrollback_lines 65536
+vte.fork_command("bluetoothctl", [ "bluetoothctl"] ) 
+
+# VBox for stacking widgets
+lvb = Gtk::VBox.new
+lvb.pack_start_defaults(msg_label)
+lvb.pack_start_defaults(entry)
+# lvb.pack_start_defaults(progressframe)
+
+window = Gtk::Window.new
+window.signal_connect("delete_event") {
+        puts "delete event occurred"
+	vte.feed_child("quit\n")
+        false
+}
+
+window.signal_connect("destroy") {
+        puts "destroy event occurred"
+	vte.feed_child("quit\n")
+        Gtk.main_quit
+}
+
+window.set_title("LessLinuxBlueTool")
+window.signal_connect("show") {  connect_everything(vte, msg_label) }
+# window.window_position = Gtk::Window::POS_CENTER_ALWAYS
+window.add lvb
+window.show_all
+Gtk.main
