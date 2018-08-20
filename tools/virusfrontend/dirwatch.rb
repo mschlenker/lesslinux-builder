158baecf678234e24f1634ba3ac88b9d182fac8e
Mattias Schlenker <ms@mattiasschlenker.de>
2018-08-20 11:47:07 +0200

    fix call

--- tools/virusfrontend/dirwatch.rb
+++ tools/virusfrontend/dirwatch.rb
@@ -4,6 +4,6 @@
 require 'rb-inotify'
 notifier = INotify::Notifier.new
 notifier.watch(ARGV[0], :close_write) { |event|
-	system('ruby /usr/share/lesslinux/avfrontend/autoscan.rb "#{ARGV[0]}/#{event.name}"')
+	system("ruby /usr/share/lesslinux/avfrontend/autoscan.rb \"#{ARGV[0]}/#{event.name}\"")
 }
 notifier.run
